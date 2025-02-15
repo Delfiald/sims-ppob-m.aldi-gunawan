750f2137859bf05bb61232b36246827277f11b98
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2025-02-15 10:32:11 +0700

    Update edit profile behavior
    - Add `editable` state to toggle profile form editability
    - Change "Edit Profile" button to toggle between "Simpan" and "Batalkan"
    - Disable input fields when `editable` is `false`, enable when `true`
    - Show "Simpan" and "Batalkan" buttons when `editable` is `true`, otherwise show "Edit Profile" and "Logout"

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,8 +1,12 @@
+"use client";
+
 import styles from "@/app/account/account.module.css";
-import { AtSign, Edit, Pencil, User } from "lucide-react";
+import { AtSign, Pencil, User } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Account() {
+ const [editable, setEditable] = useState(false);
  return (
   <div className={styles.account}>
    <div className={`${styles["input"]} ${styles["profile-picture"]}`}>
@@ -25,27 +29,40 @@ export default function Account() {
      <label htmlFor="email">Email</label>
      <div className={styles["input-wrapper"]}>
       <AtSign size={18} />
-      <input type="email" name="email" id="email" />
+      <input type="email" name="email" id="email" disabled={!editable} />
      </div>
     </div>
     <div className={`${styles["input"]} ${styles.surname}`}>
      <label htmlFor="surname">Nama Depan</label>
      <div className={styles["input-wrapper"]}>
       <User size={18} />
-      <input type="text" name="surname" id="surname" />
+      <input type="text" name="surname" id="surname" disabled={!editable} />
      </div>
     </div>
     <div className={`${styles["input"]} ${styles["last-name"]}`}>
      <label htmlFor="last-name">Nama Belakang</label>
      <div className={styles["input-wrapper"]}>
       <User size={18} />
-      <input type="text" name="last-name" id="last-name" />
+      <input type="text" name="last-name" id="last-name" disabled={!editable} />
      </div>
     </div>
-    <button type="submit" className={styles.submit}>
-     Edit Profil
-    </button>
-    <button className={styles.logout}>Logout</button>
+    {!editable ? (
+     <>
+      <button onClick={() => setEditable(true)} className={styles.edit}>
+       Edit Profile
+      </button>
+      <button className={styles.logout}>Logout</button>
+     </>
+    ) : (
+     <>
+      <button onClick={() => setEditable(false)} className={styles.submit}>
+       Simpan
+      </button>
+      <button onClick={() => setEditable(false)} className={styles.cancel}>
+       Batalkan
+      </button>
+     </>
+    )}
    </div>
   </div>
  );
