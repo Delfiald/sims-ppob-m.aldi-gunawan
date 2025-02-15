ae2fab6db23760ae42c13acd1a6d70ef92693032
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2025-02-15 21:54:53 +0700

    Create login page and auth layout
    - Create AuthLayout for login and registration pages
    - AuthLayout has two sections: auth content and illustration
    - Illustration section is an image taking more than half of the screen
    - Create login page with three sections: header, form, and action
    - Form contains two inputs: email and password
    - Add visibility state for password input
    - Password input has a toggler to switch visibility and change eye icon
    - Login action contains a submit button and a registration link
    - Registration link navigates to the registration page when clicked
    - Add feedback when login fails, including a close button

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,3 +1,18 @@
+import styles from "./authLayout.module.css";
+import Image from "next/image";
+
 export default function AuthLayout({ children }) {
- return <>{children}</>;
+ return (
+  <main className={styles.auth}>
+   <section className={styles["auth-content"]}>{children}</section>
+   <section className={styles["auth-illustration"]}>
+    <Image
+     src={"/Illustrasi Login.png"}
+     alt="Illustration"
+     width={1024}
+     height={1024}
+    />
+   </section>
+  </main>
+ );
 }
--- src/app/account/layout.js
+++ src/app/account/layout.js
@@ -1,5 +1,4 @@
 import Navbar from "@/components/Navbar/Navbar";
-import UserSection from "@/components/UserSection/UserSection";
 
 export default function AccountLayout({ children }) {
  return (
